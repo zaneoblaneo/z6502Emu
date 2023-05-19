da9155edb517ba69681c327658b1ffd6bdf494c5
Ashton Kent <ashton@yamom.net>
2023-05-19 03:00:15 -0400

    Implemented a few more opcodes, and refactored the setting of the flag registers.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,14 @@ struct Cpu6502{
 }
 
 impl Cpu6502{
+    const N_FLAG: u8 = 0b1000_0000;
+    const V_FLAG: u8 = 0b0100_0000;
+    const B_FLAG: u8 = 0b0001_0000;
+    const D_FLAG: u8 = 0b0000_1000;
+    const I_FLAG: u8 = 0b0000_0100;
+    const Z_FLAG: u8 = 0b0000_0010;
+    const C_FLAG: u8 = 0b0000_0001;
+
     fn new() -> Self {
         Cpu6502 {
             a: 0,
@@ -34,93 +42,116 @@ impl Cpu6502{
         self.status = 0x24;
     }
     
-    fn brk(&mut self) {
-        unimplemented!("BRK")
-    }
-
-    fn set_n_flag(&mut self, d: bool) {
+    fn set_flag(&mut self, flag_mask: u8, d: bool){
         if d {
-            self.status |= 0b1000_0000;
+            self.status |= flag_mask;
         } else {
-            self.status &= 0b0111_1111;
+            self.status &= !flag_mask;
         }
     }
 
-    fn set_v_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0100_0000;
-        } else {
-            self.status &= 0b1011_1111;
-        }
-    }
-    
-    fn set_b_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0001_0000;
-        } else {
-            self.status &= 0b1110_1111;
-        }
+    fn push_u8(&mut self, d: u8){
+        self.memory[self.sp as usize] = d;
+        self.sp -= 1u8;
     }
 
-    fn set_d_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0000_1000;
-        } else {
-            self.status &= 0b1111_0111;
-        }
-    }
-
-    fn set_i_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0000_0100;
-        } else {
-            self.status &= 0b1111_1011;
-        }
-    }
-    
-    fn set_z_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0000_0010;
-        } else {
-            self.status &= 0b1111_1101;
-        }
-    }
-
-    fn set_c_flag(&mut self, d: bool) {
-        if d {
-            self.status |= 0b0000_0001;
-        } else {
-            self.status &= 0b1111_1110;
-        }
-    }
-    
-    fn ldx_imm(&mut self, d: u8) {
-        self.set_n_flag(d > 0b1000_0000);
-        self.set_z_flag(d == 0);
-        self.x = d;
-        self.pc += 2;
-    }
-
-    fn ora_imm(&mut self, d: u8) {
-        self.set_n_flag(d > 0b1000_0000);
-        self.set_z_flag(d == 0);
-        self.a |= d;
+    fn push_u16(&mut self, d: u16){
+        self.memory[self.sp as usize] = ((d >> 8) & 0xffu16) as u8;
+        self.memory[self.sp as usize - 1usize] = d as u8;
+        self.sp -= 2u8;
     }
 
     fn run(&mut self) {
         loop {
+            let old_pc: u16 = self.pc;
             let opcode = self.memory[self.pc as usize];
+            let b: u8 = self.memory[self.pc as usize + 1usize];
+            let _hw: u16 = ((self.memory[self.pc as usize + 2usize] as u16) 
+                           << 8u16) | 
+                self.memory[self.pc as usize + 1usize] as u16;
             match opcode {
-                0 => self.brk(),
-                0x13 => unimplemented!("Invalid opcode. 0x{opcode:2X}"),
-                0x18 => self.set_c_flag(false),
-                0x58 => self.set_i_flag(false),
-                0xA2 => self.ldx_imm(self.memory[self.pc as usize + 1usize]),
-                0xB8 => self.set_v_flag(false),
-                0xD8 => self.set_d_flag(false),
+                0x00 => { // BRK
+                    unimplemented!("{:04x} BRK", self.pc);
+                },
+                0x01 => { // ORA X, ind
+                    let addr: u16 = 
+                        ((self.memory[b as usize + self.x as usize] as u16) 
+                        << 8u16) | self.memory[b as usize 
+                            + self.x as usize + 1usize] as u16;
+                    self.set_flag(Self::N_FLAG, addr > 0b1000_0000);
+                    self.set_flag(Self::Z_FLAG, addr == 0);
+                    self.a |= self.memory[addr as usize];
+                    self.pc += 2;
+                }
+                0x05 => { // ORA zpg
+                    let d = self.memory[b as usize];
+                    self.set_flag(Self::N_FLAG, d > 0b1000_0000);
+                    self.set_flag(Self::Z_FLAG, d == 0);
+                    self.a |= self.memory[d as usize];
+                    self.pc += 2;
+                },
+                0x06 => { // ASL zpg
+                    let mut d: u8 = self.memory[b as usize];
+                    self.set_flag(Self::C_FLAG, (d & 0b1000_0000) != 0);
+                    d <<= 1;
+                    self.set_flag(Self::Z_FLAG, d == 0);
+                    self.set_flag(Self::N_FLAG, d > 0b1000_0000);
+                    self.memory[b as usize] = d;
+                    self.pc += 2;
+
+                },
+                0x08 => { // PHP
+                    self.push_u8(self.status);
+                    self.pc += 1;
+                }
+                0x09 => { // ORA #
+                    self.set_flag(Self::N_FLAG, b > 0b1000_0000);
+                    self.set_flag(Self::Z_FLAG, b == 0);
+                    self.a |= b;
+                    self.pc += 2;
+                },
+                0x0A => { // ASL A
+                    self.set_flag(Self::C_FLAG, (self.a & 0b1000_0000) != 0);
+                    self.a <<= 1u8;
+                    self.set_flag(Self::Z_FLAG, self.a == 0);
+                    self.set_flag(Self::N_FLAG, self.a > 0b1000_0000);
+                    self.pc += 1;
+                },
+                0x0D => { // ORA abs
+                    unimplemented!("TODO: implement ORA abs.");
+                }
+                0x18 => { // CLC
+                    self.set_flag(Self::C_FLAG, false);
+                    self.pc += 1;
+                },
+                0x58 => { // CLI
+                    self.set_flag(Self::I_FLAG, false);
+                    self.pc += 1;
+                },
+                0xA2 => { // LDX #
+                    self.set_flag(Self::N_FLAG, b > 0b1000_0000);
+                    self.set_flag(Self::Z_FLAG, b == 0);
+                    self.x = b;
+                    self.pc += 2;
+                },
+                0xB8 => { // CLV
+                    self.set_flag(Self::V_FLAG, false);
+                    self.pc += 1;
+                },
+                0xD8 => { // CLD
+                    self.set_flag(Self::D_FLAG, false);
+                    self.pc += 1;
+                },
+                0xE0 => { // CPX #
+                    self.set_flag(Self::N_FLAG, (b & 0b1000_0000u8) != 0u8);
+                    self.set_flag(Self::Z_FLAG, b == 0);
+                    self.set_flag(Self::C_FLAG, self.x < b);
+                    self.pc += 2;
+                },
                 0xff => break, // CUSTOM HALT
                 _ => unimplemented!("0x{opcode:2X}"),
             }
+            assert!(old_pc != self.pc, "{opcode:02X} does not increment pc.");
         }
     }
 
@@ -143,10 +174,14 @@ impl Cpu6502{
 
 fn main() {
     let mut cpu: Cpu6502 = Cpu6502::new();
+    // set reset vector to 0x8000
+    cpu.memory[0xfffc] = 0x00;
+    cpu.memory[0xfffd] = 0x80;
     cpu.reset();
-    cpu.memory[0] = 0xA2;
-    cpu.memory[1] = 0xf0;
-    cpu.memory[2] = 0xff;
+    // set instructions
+    cpu.memory[0x8000] = 0xA2;
+    cpu.memory[0x8001] = 0xf0;
+    cpu.memory[0x8002] = 0xff;
     cpu.print_regs();
     cpu.run();
     cpu.print_regs();
