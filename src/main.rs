281a0ce59bff9206fe6ec4cf36c72e1b193968e6
zaneoblaneo <zaneoblaneo@gmail.com>
2023-05-28 10:55:07 +0000

    added a few more instructions.

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,11 @@ impl Cpu6502{
         self.status = 0x24;
     }
 
-    fn check_neg_u8(self, num: u8) -> bool {
+    fn check_neg_u8(&self, num: u8) -> bool {
         num & 0b1000_0000 != 0
     }
     
-    fn check_neg_u16(self, num: u16) -> bool {
+    fn check_neg_u16(&self, num: u16) -> bool {
         num & 0b1000_0000_0000_0000 != 0
     }
 
@@ -58,13 +58,13 @@ impl Cpu6502{
         }
     }
 
-    fn get_flag(self, flag_mask: u8) -> bool{
+    fn get_flag(&self, flag_mask: u8) -> bool{
         self.status & flag_mask != 0
     }
     
     /// returns the flag selected by `flag_mask`
     /// to the user as either a `0` or a `1`
-    fn get_flag_u8(&mut self, flag_mask: u8) -> u8{
+    fn get_flag_u8(&self, flag_mask: u8) -> u8{
         match flag_mask{
             0b1000_0000 => (self.status >> 7) & 1,
             0b0100_0000 => (self.status >> 6) & 1,
@@ -75,6 +75,66 @@ impl Cpu6502{
             0b0000_0001 => self.status & 1,
             _ => unreachable!(""),
         }
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// abs,X addressing mode
+    fn get_addr_abs_x(&self) -> usize{
+        let hw: usize = (self.memory[self.pc as usize + 1] as usize) << 8
+            | self.memory[self.pc as usize + 2] as usize;
+        hw + self.x as usize + self.get_flag_u8(Self::C_FLAG) as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// abs,Y addressing mode
+    fn get_addr_abs_y(&self) -> usize{
+        let hw: usize = (self.memory[self.pc as usize + 1] as usize) << 8
+            | self.memory[self.pc as usize + 2] as usize;
+        hw + self.y as usize + self.get_flag_u8(Self::C_FLAG) as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// ind addressing mode
+    fn get_addr_ind(&self) -> usize{
+        let hw: usize = (self.memory[self.pc as usize + 1] as usize) << 8
+            | self.memory[self.pc as usize + 2] as usize;
+        (self.memory[hw] as usize) << 8 | self.memory[hw + 1] as usize
+    }
+
+    /// A helper function that returns the index into `memory` using the
+    /// X,ind addressing mode
+    fn get_addr_x_ind(&self) -> usize{
+        let b: usize = self.memory[self.pc as usize + 1] as usize;
+        (self.memory[b + self.x as usize] as usize) << 8 
+            | self.memory[b + self.x as usize + 1] as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the 
+    /// ind,Y addressing mode
+    fn get_addr_ind_y(&self) -> usize{
+        let b: usize = self.memory[self.pc as usize + 1] as usize;
+        ((self.memory[b] as usize) << 8 | self.memory[b + 1] as usize) 
+            + self.y as usize + self.get_flag_u8(Self::C_FLAG) as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// rel addressing mode
+    fn get_addr_rel(&self) -> usize{
+        self.pc as usize + self.memory[self.pc as usize + 1] as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// zpg,X addressing mode
+    fn get_addr_zpg_x(&self) -> usize{
+        let b: usize = self.memory[self.pc as usize + 1] as usize;
+        b + self.x as usize
+    }
+    
+    /// A helper function that returns the index into `memory` using the
+    /// zpg,Y addressing mode
+    fn get_addr_zpg_y(&self) -> usize{
+        let b: usize = self.memory[self.pc as usize + 1] as usize;
+        b + self.y as usize
     }
 
     fn push_u8(&mut self, d: u8){
@@ -137,6 +197,13 @@ impl Cpu6502{
         out
     }
 
+    /// Defined as `a` = `a` EOR `data` while setting the `N_FLAG` and `Z_FLAG`
+    fn op_eor(&mut self, data: u8){
+        self.a ^= data;
+        self.set_flag(Self::N_FLAG, self.check_neg_u8(self.a));
+        self.set_flag(Self::Z_FLAG, self.a == 0);
+    }
+
     fn run(&mut self) {
         loop {
             let old_pc: u16 = self.pc;
@@ -156,8 +223,7 @@ impl Cpu6502{
                     self.pc += 1;
                 },
                 0x01 => { // ORA X, ind
-                    let addr: usize = (b as usize + self.x as usize) << 8 
-                        | (b as usize + self.x as usize + 1usize);
+                    let addr: usize = self.get_addr_x_ind();
                     self.op_ora(self.memory[addr]);
                     self.pc += 2;
                 }
@@ -202,21 +268,19 @@ impl Cpu6502{
                     }
                 },
                 0x11 => { // ORA ind,Y
-                    let data: u8 = self.memory[b as usize] 
-                        + self.y + self.get_flag_u8(Self::C_FLAG);
+                    let data: u8 = self.memory[self.get_addr_ind_y()];
                     self.op_ora(data);
                     self.pc += 2;
                 },
                 0x15 => { // ORA zpg,X
-                    let data: u8 = self.memory[b as usize + self.x as usize];
+                    let data: u8 = self.memory[self.get_addr_zpg_x()];
                     self.op_ora(data);
                     self.pc += 2;
                 },
                 0x16 => { // ASL zpg,X
-                    let data: u8 
-                        = self.memory[b as usize + self.x as usize];
+                    let data: u8 = self.memory[self.get_addr_zpg_x()];
                     let data: u8 = self.op_asl(data);
-                    self.memory[b as usize + self.x as usize] = data;
+                    self.memory[self.get_addr_zpg_x()] = data;
                     self.pc += 2;
                 },
                 0x18 => { // CLC
@@ -224,22 +288,18 @@ impl Cpu6502{
                     self.pc += 1;
                 },
                 0x19 => { // ORA abs,Y
-                    let addr: usize = hw as usize + self.y as usize 
-                        + self.get_flag_u8(Self::C_FLAG) as usize;
+                    let addr: usize = self.get_addr_abs_y();
                     self.op_ora(self.memory[addr]);
                     self.pc += 3;
                 },
                 0x1D => { // ORA abs,X
-                    let addr: usize = hw as usize + self.x as usize
-                        + self.get_flag_u8(Self::C_FLAG) as usize;
+                    let addr: usize = self.get_addr_abs_x();
                     self.op_ora(self.memory[addr]);
                     self.pc += 3;
                 },
                 0x1E => { // ASL abs,X
-                    let addr: usize = hw as usize + self.x as usize 
-                        + self.get_flag_u8(Self::C_FLAG) as usize;
-                    let data: u8 = self.op_asl(self.memory[addr]);
-                    self.memory[addr] = data;
+                    let addr: usize = self.get_addr_abs_x();
+                    self.memory[addr] = self.op_asl(self.memory[addr]);
                     self.pc += 3;
                 },
                 0x20 => { // JSR abs
@@ -247,10 +307,7 @@ impl Cpu6502{
                     self.pc = hw;
                 },
                 0x21 => { // AND X,ind
-                    let index: usize = b as usize + self.x as usize;
-                    let addr: usize 
-                        = (self.memory[index] as usize) << 8 
-                        | (self.memory[index + 1usize] as usize);
+                    let addr: usize = self.get_addr_x_ind();
                     self.op_and(self.memory[addr]);
                     self.pc += 2;
                 },
@@ -265,7 +322,7 @@ impl Cpu6502{
                     self.op_and(self.memory[b as usize]);
                     self.pc += 2;
                 },
-                0x26 => { // ROL zpg (sets C, Z, and N)
+                0x26 => { // ROL zpg
                     self.memory[b as usize] 
                         = self.op_rol(self.memory[b as usize]);
                     self.pc += 2;
@@ -346,6 +403,17 @@ impl Cpu6502{
                         + self.get_flag_u8(Self::C_FLAG) as usize;
                     self.memory[addr] = self.op_rol(self.memory[addr]);
                     self.pc += 3;
+                },
+                0x40 => { // RTI
+                    self.status = self.pop_u8() & 0b1111_0111;
+                    self.pc = self.pop_u16();
+                },
+                0x41 => { // EOR X,ind
+                    let index: usize = b as usize + self.x as usize;
+                    let addr: usize = (self.memory[index] as usize) << 8
+                        | (self.memory[index + 1usize] as usize);
+                    self.op_eor(self.memory[addr]);
+                    self.pc += 2;
                 },
                 0x58 => { // CLI
                     self.set_flag(Self::I_FLAG, false);
